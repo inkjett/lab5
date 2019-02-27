3161f86f4b2b341138be099ace01687213f4dae2
inkjett <mail22sms@gmail.com>
2019-02-27 22:57:47 +0500

    запилил вывод в датагрид

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -217,6 +217,7 @@
             this.button4.TabIndex = 17;
             this.button4.Text = "Сгенерировать массив и вывести его";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridView1
             // 
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -8,11 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Collections;
 
 namespace lab5
 {
     public partial class Form1 : Form
     {
+        //переменные 
+        string text_from_file = "";
+        Int32[,] arr_main;
+        Int32 count_of_word;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -23,10 +30,7 @@ namespace lab5
             dataGridView1.ColumnHeadersVisible = false;
             dataGridView1.RowHeadersVisible = false;
         }
-        //переменные 
-        string text_from_file = "";
-        Int32[,] arr_main;
-        Int32 count_of_word;
+
      
         //методы
         public void method_read_from_file (ref string text_out) // запись текста в строку
@@ -77,17 +81,34 @@ namespace lab5
 
         public void method_gen_arr(TextBox size1, TextBox size2, TextBox Value_min, TextBox Value_max, ref int [,] arr_out )//генерация массива данных
         {
-            Int32[,] temp_array;
-            temp_array = new int [Convert.ToInt32(size1), Convert.ToInt32(size2)];
+            arr_out = new int [Int32.Parse(size1.Text), Int32.Parse(size2.Text)];
             Random rnd = new Random();
-            for (int i=0; i<temp_array.GetLength(0);i++)
+            for (int i=0; i< arr_out.GetLength(0);i++)
             {
-                for(int j=0;j<temp_array.GetLength(0);j++)
+                for(int j=0;j< arr_out.GetLength(0);j++)
                 {
-                    temp_array[i, j] = rnd.Next(Convert.ToInt32(Value_min), Convert.ToInt32(Value_max));
+                    arr_out[i, j] = rnd.Next(Int32.Parse(Value_min.Text), Int32.Parse(Value_max.Text));
                 }
             }
         }
+
+        public void metthod_arr_to_grid (int [,] arr_in,ref DataGridView out_DataGrid)
+        {
+            out_DataGrid.RowCount = arr_in.GetLength(0);
+            out_DataGrid.ColumnCount = arr_in.GetLength(0);
+            for (int i=0;i<arr_in.GetLength(0);i++)
+            {
+                for(int j=0;j< arr_in.GetLength(1);j++)
+                {
+                    out_DataGrid.Rows[i].Cells[j].Value = String.Format("{0}", arr_in[i,j]);
+                }
+
+            }
+
+
+
+        }
+        
 
 
         //форма
@@ -113,6 +134,13 @@ namespace lab5
         private void button3_Click(object sender, EventArgs e)
         {
             method_save_to_file(label4.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            arr_main = new Int32[Int32.Parse(textBox3.Text),Int32.Parse(textBox4.Text)];
+            method_gen_arr(textBox3,textBox4,textBox5,textBox6,ref arr_main);
+            metthod_arr_to_grid(arr_main,ref dataGridView1);
         }
     }
 }
