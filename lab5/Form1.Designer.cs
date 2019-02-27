02cdfb453f8e2a320973e47020d2fb7f1e46f2d2
inkjett <mail22sms@gmail.com>
2019-02-27 17:01:23 +0500

    Разрабатываю второе задаени

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -39,6 +39,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -119,11 +131,117 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(148, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Записать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(501, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(501, 265);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "3";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(648, 224);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(648, 265);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.Text = "20";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(498, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Количество строк";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(498, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Количество столбцов";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(645, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Минимальное значение";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(645, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Максимальное значение";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(501, 291);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(247, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Сгенерировать массив и вывести его";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(487, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(286, 160);
+            this.dataGridView1.TabIndex = 18;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(799, 504);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -135,6 +253,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,6 +271,17 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -20,27 +20,47 @@ namespace lab5
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
         }
         //переменные 
         string text_from_file = "";
+        Int32[,] arr_main;
         Int32 count_of_word;
+     
         //методы
         public void method_read_from_file (ref string text_out) // запись текста в строку
         {
             openFileDialog1.Filter = "Text files (*.txt)|*.txt";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                string Name_file = openFileDialog1.FileName;
-                var R = new StreamReader(Name_file, Encoding.GetEncoding(1251));
-                text_out = R.ReadLine();
+                string Name_file_to_read = openFileDialog1.FileName;
+                using (var R = new StreamReader(Name_file_to_read, Encoding.GetEncoding(1251)))
+                {
+                    text_out = R.ReadLine();
+                }
             }
 
         }
 
-
-        public void method_count_of_words (string text_in,string word_to_find,ref Int32 count)
+        public void method_save_to_file(string text_in)// запись текста в файл
         {
+
+                saveFileDialog1.Filter = "Text files (*.txt)|*.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                        string Name_file_to_write = saveFileDialog1.FileName;                        
+                        using (StreamWriter W = new StreamWriter(Name_file_to_write, true, Encoding.GetEncoding(1251)))
+                        {
+                            W.WriteLine(text_in);
+                        }
+                }
+        }
+
+
+        public void method_count_of_words (string text_in,string word_to_find,ref Int32 count)// подсчет слов в тексте
+        {
+            count = 0;
             text_in.Trim();
             Int32 lengh_word = word_to_find.Length;
             for (int i=0; i<=text_in.Length-lengh_word; i++ )
@@ -55,8 +75,22 @@ namespace lab5
 
         }
 
+        public void method_gen_arr(TextBox size1, TextBox size2, TextBox Value_min, TextBox Value_max, ref int [,] arr_out )//генерация массива данных
+        {
+            Int32[,] temp_array;
+            temp_array = new int [Convert.ToInt32(size1), Convert.ToInt32(size2)];
+            Random rnd = new Random();
+            for (int i=0; i<temp_array.GetLength(0);i++)
+            {
+                for(int j=0;j<temp_array.GetLength(0);j++)
+                {
+                    temp_array[i, j] = rnd.Next(Convert.ToInt32(Value_min), Convert.ToInt32(Value_max));
+                }
+            }
+        }
 
-        //кнопки формы
+
+        //форма
         private void button1_Click(object sender, EventArgs e)// вывод текста в текстбокс
         {
             method_read_from_file(ref text_from_file);
@@ -74,6 +108,11 @@ namespace lab5
         {
             method_count_of_words(text_from_file, textBox2.Text ,ref count_of_word);
             label4.Text = Convert.ToString(count_of_word);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            method_save_to_file(label4.Text);
         }
     }
 }
